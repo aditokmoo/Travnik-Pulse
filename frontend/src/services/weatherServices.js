cdf21e872f4973183a77e099f37ccebf998ea2cf
Havock48 <adit.github@gmail.com>
2025-03-29 11:19:14 +0100

    fix slash problem

--- frontend/src/services/weatherServices.js
+++ frontend/src/services/weatherServices.js
@@ -4,7 +4,7 @@ const API_KEY = "7890d190f2b467ed6b8866f49be5dd2e";
 // Get weather details in travnik
 export async function getCityWeather() {
     try {
-        const res = await fetch(`${API_URL}/${API_KEY}`);
+        const res = await fetch(`${API_URL}${API_KEY}`);
         const data = await res.json();
         return data;
     } catch (error) {
